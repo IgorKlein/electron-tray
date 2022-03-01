c82e5451c404be36486722a342a446de4c2cfe75
Igor Klein <igor.klein.sl@gmail.com>
2022-03-01 19:41:46 -0300

    Icon tray implemented ok.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { resolve } = require('path')
 const { app, Menu, Tray } = require('electron')
 
 app.on('ready', () => {
-    const tray = new Tray(resolve(__dirname, 'assets', 'tray-icon'));
+    const tray = new Tray(resolve(__dirname, 'assets', 'tray-icon.png'));
 
     const contextMenu = Menu.buildFromTemplate([
         { label: 'Item1', type: 'radio', checked: true }
